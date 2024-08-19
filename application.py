e48c18d059b3ff21630a236d7536c35cde42c80e
Altamash Ajaz <altamashajaz101@gmail.com>
2024-08-20 05:06:09 +0530

    App.py

--- application.py
+++ application.py
@@ -22,23 +22,27 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        data = CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('race_ethnicity'),
-            parental_education=request.form.get('parental_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            writing_score=float(request.form.get('writing_score')),
-            reading_score=float(request.form.get('reading_score'))
-        )
+        try:
+            data = CustomData(
+                gender=request.form.get('gender'),
+                race_ethnicity=request.form.get('race_ethnicity'),
+                parental_education=request.form.get('parental_education'),
+                lunch=request.form.get('lunch'),
+                test_preparation_course=request.form.get('test_preparation_course'),
+                writing_score=float(request.form.get('writing_score')),
+                reading_score=float(request.form.get('reading_score'))
+            )
 
-        pred_df = data.get_data_as_dataframe()
-        print(pred_df)
+            pred_df = data.get_data_as_dataframe()
+            print(pred_df)
 
-        predict_pipeline = PredictPipeline()
-        results = predict_pipeline.predict(pred_df)
+            predict_pipeline = PredictPipeline()
+            results = predict_pipeline.predict(pred_df)
 
-        return render_template('home.html', results=results[0])
+            return render_template('home.html', results=results[0])
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            return render_template('home.html', results="Error occurred during prediction.")
 
 if __name__=='__main__':
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0")
